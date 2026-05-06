67c94f9b69df6e368528ce2767b316fc5aa5df23
atteszp-beep <atteszp@gmail.com>
2026-05-06 20:15:14 +0200

    auto update

--- public/client.js
+++ public/client.js
@@ -1,18 +1,1 @@
-const socket = new WebSocket("ws://localhost:3000");
-
-socket.onopen = () => {
-    console.log("Connected to server");
-    socket.send(JSON.stringify({ type: "join", name: "Player1" }));
-};
-
-socket.onmessage = (event) => {
-    const data = JSON.parse(event.data);
-    console.log("Message from server:", data);
-};
-
-document.addEventListener("keydown", (e) => {
-    socket.send(JSON.stringify({
-        type: "move",
-        key: e.key
-    }));
-});
+console.log('client.js loaded');
